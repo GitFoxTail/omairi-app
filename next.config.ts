1915094e5e3b10e318da0de8bc181d2aad6eee9d
Yuji <heiseirider20@yahoo.co.jp>
2024-12-12 22:49:36 +0900

    Appserviceへの環境変数PORTとNODE_ENVも追加

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'standalone',
+  experimental: {
+    serverActions: {
+      //デフォルト
+    }
+  }
 };
 
 export default nextConfig;
